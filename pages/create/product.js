721d8ac4c66cca04a8d989d64f7c4f20c9ff105e
maher <gomemahero.com>
2022-05-13 21:07:38 +0300

    error try to send data in Formdata

--- pages/create/product.js
+++ pages/create/product.js
@@ -5,20 +5,31 @@ import { postData, getData } from "../..//utils/fetchdata";
 const Product = () => {
 
 const [products, setProducts] = useState();
-
+const [images, setImages] = useState([]);
 
 // send data to server
 
 const handleSubmit = (e) => {
 
     e.preventDefault();
-    const data = {
-        name: "product name",
-        price: "product price",
-        inStock: "product inStock",
-        description: "product description",};
+    // const data = {
+    //     name: "product name",
+    //     price: "product price",
+    //     inStock: "product inStock",
+    //     description: "product description",};
 
-    postData("products", data).then((data) => {
+    const formData = new FormData();
+        formData.append("name", "product name");
+        formData.append("price", "product price");
+        formData.append("inStock", "product inStock");
+        formData.append("description", "product description");
+        formData.append("content", "product content");
+        formData.append("category", "product category");
+        formData.append("images", images);
+
+        console.log(formData, "form data");
+
+    postData("products", formData).then((data) => {
 
         console.log(data);
     });
@@ -35,6 +46,23 @@ const handleSubmit = (e) => {
 
   ]);
 
+
+// handle imagechange
+
+const handleImageChange = (e) => {
+
+    e.preventDefault();
+    const files = e.target.files;
+    setImages(files);
+    console.log('image files here---->',files);
+    const data = new FormData();
+    data.append("file", files);
+
+};
+
+
+
+
   return (
     <div>
       <h1>Product</h1>
@@ -45,6 +73,16 @@ const handleSubmit = (e) => {
 
 <div>
     
+
+<form>
+    
+    <input type="file" multiple  onChange={handleImageChange}  name="images"  />
+
+
+</form>
+
+
+
 <button type="submit"  onClick={handleSubmit}>
 
 Send data to server
