eb58a24e93339e75e8b6a44d59d00d7c5bb191ac
maher <gomemahero.com>
2022-05-13 09:56:02 +0300

    connect to mongodb

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import  mongodb from '../../server/mongodb';
 
-mongodb();
+mongodb();  //--> connected to mongodb successfully
 
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
